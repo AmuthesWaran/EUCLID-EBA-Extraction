31eb61b3c986fac04d02ea3ca854c5c912eb13f2
Amutheswaran <amutheswaran@outlook.com>
2023-01-28 12:14:54 +0530

    minor amendments and powerquery transformations added

--- src/components/Extract.jsx
+++ src/components/Extract.jsx
@@ -1,35 +1,34 @@
-import axios from 'axios';
 import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
-import React, { useState } from 'react';
+import React from 'react';
+import jsonData from '../server/CreditInstitutionsRegister.json'
 
 const Extract = () => {
   
-    const [data, setData] = useState([])
-    console.log(data);
+    console.log(jsonData)
+    const data = jsonData.data
+    // console.log(data);
 
-    const loadData = () => {
+    // const loadData = () => {
 
-        let url = `http://localhost:3000/data`;
+        // let url = `http://localhost:3000/data`;
 
-        axios.get(url)
-             .then(response=> setData(response.data) )
-             .catch(error=>console.log(error))  
+        // axios.get(url)
+        //      .then(response=> setData(response.data) )
+        //      .catch(error=>console.log(error))  
 
-             
+        // axios.get(`https://euclid.eba.europa.eu/register/api/search/entities?t=1665061336642`)
+        //      .then(response => console.log(response))
+        //      .catch(error => console.log(error))
 
 
-        axios.get(`https://euclid.eba.europa.eu/register/api/search/entities?t=1665061336642`)
-             .then(response => console.log(response))
-             .catch(error => console.log(error))
-    }
+    // }
 
     // response.data[0]._payload.Properties
 
     return (
     <div>
 
-    <Button variant="primary" onClick={loadData}>Click here</Button>
+    {/* <Button variant="primary" onClick={loadData}>Click here</Button> */}
     <Table striped bordered hover>
         {/* <table border="1"> */}
             <thead>
